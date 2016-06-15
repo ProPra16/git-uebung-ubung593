e175756600ef7b8f78a54c2406138bb5d2553466
testhhupropra <test@test.test>
2016-06-15 15:42:59 +0200

    added Time Button

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,21 @@
 package de.hhu.propra16;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+
+    @FXML private Label timeLabel;
+
+    @FXML
+    protected void handleTimeButtonAction(ActionEvent event) {
+        timeLabel.setText(LocalTime.now().toString());
+    }
 }
