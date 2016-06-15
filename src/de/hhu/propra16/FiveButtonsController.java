54ac4425c7b711ef75bdc1c9510a4cbc90c12864
testhhupropra <test@test.test>
2016-06-15 15:50:22 +0200

    added File Button

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,17 @@
 package de.hhu.propra16;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+
+    @FXML
+    public void handleFileButton(ActionEvent actionEvent) {
+
+    }
+
 }
