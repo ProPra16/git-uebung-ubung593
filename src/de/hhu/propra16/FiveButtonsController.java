ee0620e1a84c18cb5f4319019f174eb8c7c365c4
a <n@e.com>
2016-06-15 16:02:43 +0200

    add close button

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,14 @@
 package de.hhu.propra16;
 
+import javafx.fxml.FXML;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+    @FXML
+    void handleCloseButton() {
+        System.exit(0);
+    }
+
 }
