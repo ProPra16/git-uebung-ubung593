52ac43dd24b500eb6ae1e1c72ac15fa613c2d7a6
dfa <fickdich>
2016-06-15 15:55:29 +0200

    alertchange

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,18 @@
 package de.hhu.propra16;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+    @FXML Button alert;
+
+     public void alert(){
+         Alert alert = new Alert(Alert.AlertType.ERROR);
+         alert.setContentText("Too many Buttons");
+         alert.show();
+     }
 }
