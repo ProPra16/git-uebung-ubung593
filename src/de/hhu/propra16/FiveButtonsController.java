568d3e7a4a3184fe7cff98610e98cafaa98aaf32
Can <a@b.de>
2016-06-15 15:59:41 +0200

    hallo

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,20 @@
 package de.hhu.propra16;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+    @FXML
+    Button background;
+    @FXML
+    GridPane mainPane;
+
+    @FXML
+    public void changeBackground(){
+        mainPane.setStyle("-fx-background: #0000FF");
+    }
 }
